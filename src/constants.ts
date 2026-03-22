358497bc2b978650c77b42fd05cbd90fc771b62f
Mo-Spring <110386992+Mo-Spring@users.noreply.github.com>
2026-03-22 11:08:42 +0800

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -20,74 +20,74 @@ export const HK_INDUSTRIES: Industry[] = [
   {
     id: 'hk-tech', nm: '资讯科技', ic: '💻', ev: 'low', market: 'HK', l2: [
       { nm: '互联网', cs: [{ c: '00700', n: '腾讯控股', market: 'HK' }, { c: '09988', n: '阿里巴巴-W', market: 'HK' }, { c: '03690', n: '美团-W', market: 'HK' }] },
-      { nm: '硬件', cs: [{ c: '01810', n: '小米集团-W', market: 'HK' }] }
-    ]
+      { nm: '硬件', cs: [{ c: '01810', n: '小米集团-W', market: 'HK' }] },
+    indices: [{ c: 'HSTECH', n: '恒生科技', t: 'broad' }]
   },
   {
     id: 'hk-fin', nm: '金融业', ic: '🏦', ev: 'low', market: 'HK', l2: [
       { nm: '银行', cs: [{ c: '00005', n: '汇丰控股', market: 'HK' }, { c: '00939', n: '建设银行', market: 'HK' }] },
       { nm: '保险', cs: [{ c: '01299', n: '友邦保险', market: 'HK' }, { c: '02318', n: '中国平安', market: 'HK' }] },
-      { nm: '多元金融', cs: [{ c: '00388', n: '香港交易所', market: 'HK' }] }
-    ]
+      { nm: '多元金融', cs: [{ c: '00388', n: '香港交易所', market: 'HK' }] },
+    indices: [{ c: 'HSI', n: '恒生指数', t: 'broad' }, { c: 'HSCEI', n: '国企指数', t: 'broad' }]
   },
   {
     id: 'hk-cons', nm: '非必需性消费', ic: '🛍️', ev: 'mid', market: 'HK', l2: [
       { nm: '汽车', cs: [{ c: '01211', n: '比亚迪股份', market: 'HK' }, { c: '02015', n: '理想汽车-W', market: 'HK' }] },
       { nm: '体育用品', cs: [{ c: '02020', n: '安踏体育', market: 'HK' }, { c: '02331', n: '李宁', market: 'HK' }] },
-      { nm: '餐饮', cs: [{ c: '06862', n: '海底捞', market: 'HK' }] }
-    ]
+      { nm: '餐饮', cs: [{ c: '06862', n: '海底捞', market: 'HK' }] },
+    indices: [{ c: 'HSTECH', n: '恒生科技', t: 'broad' }]
   },
   {
     id: 'hk-cons-staples', nm: '必需性消费', ic: '🍷', ev: 'mid', market: 'HK', l2: [
       { nm: '饮料', cs: [{ c: '09633', n: '农夫山泉', market: 'HK' }, { c: '00291', n: '华润啤酒', market: 'HK' }] },
-      { nm: '乳品', cs: [{ c: '02319', n: '蒙牛乳业', market: 'HK' }] }
-    ]
+      { nm: '乳品', cs: [{ c: '02319', n: '蒙牛乳业', market: 'HK' }] },
+    indices: [{ c: 'HSI', n: '恒生指数', t: 'broad' }]
   },
   {
     id: 'hk-re', nm: '地产建筑', ic: '🏢', ev: 'low', market: 'HK', l2: [
       { nm: '香港地产', cs: [{ c: '00016', n: '新鸿基地产', market: 'HK' }, { c: '00001', n: '长和', market: 'HK' }] },
-      { nm: '内房股', cs: [{ c: '01109', n: '华润置地', market: 'HK' }, { c: '00688', n: '中国海外发展', market: 'HK' }] }
-    ]
+      { nm: '内房股', cs: [{ c: '01109', n: '华润置地', market: 'HK' }, { c: '00688', n: '中国海外发展', market: 'HK' }] },
+    indices: [{ c: 'HSI', n: '恒生指数', t: 'broad' }]
   },
   {
     id: 'hk-tele', nm: '电讯业', ic: '📡', ev: 'low', market: 'HK', l2: [
-      { nm: '电讯服务', cs: [{ c: '00941', n: '中国移动', market: 'HK' }, { c: '00728', n: '中国电信', market: 'HK' }] }
-    ]
+      { nm: '电讯服务', cs: [{ c: '00941', n: '中国移动', market: 'HK' }, { c: '00728', n: '中国电信', market: 'HK' }] },
+    indices: [{ c: 'HSI', n: '恒生指数', t: 'broad' }]
   },
   {
     id: 'hk-energy', nm: '能源业', ic: '🛢️', ev: 'low', market: 'HK', l2: [
       { nm: '石油天然气', cs: [{ c: '00883', n: '中国海洋石油', market: 'HK' }, { c: '00857', n: '中国石油股份', market: 'HK' }] },
-      { nm: '煤炭', cs: [{ c: '01088', n: '中国神华', market: 'HK' }] }
-    ]
+      { nm: '煤炭', cs: [{ c: '01088', n: '中国神华', market: 'HK' }] },
+    indices: [{ c: 'HSI', n: '恒生指数', t: 'broad' }]
   },
   {
     id: 'hk-health', nm: '医疗保健', ic: '🏥', ev: 'low', market: 'HK', l2: [
       { nm: '制药', cs: [{ c: '01093', n: '石药集团', market: 'HK' }, { c: '01177', n: '中国生物制药', market: 'HK' }] },
-      { nm: 'CXO', cs: [{ c: '02269', n: '药明生物', market: 'HK' }] }
-    ]
+      { nm: 'CXO', cs: [{ c: '02269', n: '药明生物', market: 'HK' }] },
+    indices: [{ c: 'HSTECH', n: '恒生科技', t: 'broad' }]
   },
   {
     id: 'hk-materials', nm: '原材料业', ic: '💎', ev: 'mid', market: 'HK', l2: [
       { nm: '黄金及贵金属', cs: [{ c: '02899', n: '紫金矿业', market: 'HK' }, { c: '01818', n: '招金矿业', market: 'HK' }] },
-      { nm: '工业金属', cs: [{ c: '02600', n: '中国铝业', market: 'HK' }] }
-    ]
+      { nm: '工业金属', cs: [{ c: '02600', n: '中国铝业', market: 'HK' }] },
+    indices: [{ c: 'HSI', n: '恒生指数', t: 'broad' }]
   },
   {
     id: 'hk-industrials', nm: '工业', ic: '⚙️', ev: 'mid', market: 'HK', l2: [
       { nm: '重型机械', cs: [{ c: '02039', n: '中集集团', market: 'HK' }, { c: '01157', n: '中联重科', market: 'HK' }] },
-      { nm: '航运', cs: [{ c: '01919', n: '中远海控', market: 'HK' }] }
-    ]
+      { nm: '航运', cs: [{ c: '01919', n: '中远海控', market: 'HK' }] },
+    indices: [{ c: 'HSI', n: '恒生指数', t: 'broad' }]
   },
   {
     id: 'hk-util', nm: '公用事业', ic: '💧', ev: 'mid', market: 'HK', l2: [
       { nm: '电力', cs: [{ c: '00002', n: '中电控股', market: 'HK' }, { c: '00836', n: '华润电力', market: 'HK' }] },
-      { nm: '燃气', cs: [{ c: '00384', n: '中国燃气', market: 'HK' }] }
-    ]
+      { nm: '燃气', cs: [{ c: '00384', n: '中国燃气', market: 'HK' }] },
+    indices: [{ c: 'HSI', n: '恒生指数', t: 'broad' }]
   },
   {
     id: 'hk-conglom', nm: '综合企业', ic: '🏢', ev: 'low', market: 'HK', l2: [
-      { nm: '综合', cs: [{ c: '00267', n: '中信股份', market: 'HK' }, { c: '00656', n: '复星国际', market: 'HK' }] }
-    ]
+      { nm: '综合', cs: [{ c: '00267', n: '中信股份', market: 'HK' }, { c: '00656', n: '复星国际', market: 'HK' }] },
+    indices: [{ c: 'HSI', n: '恒生指数', t: 'broad' }]
   }
 ];
 
@@ -124,19 +124,19 @@ export const INDUSTRIES: Industry[] = [
   {
     id: 'fin', nm: '非银金融', ic: '📈', ev: 'low', bk: 'BK0473', l2: [
       { nm: '证券', cs: [{ c: '600030', n: '中信证券' }, { c: '601688', n: '华泰证券' }] },
-      { nm: '保险', cs: [{ c: '601318', n: '中国平安' }, { c: '601628', n: '中国人寿' }] },
-    ]
+      { nm: '保险', cs: [{ c: '601318', n: '中国平安' }, { c: '601628', n: '中国人寿' }] },,
+    indices: [{ c: '399975', n: '证券公司', t: 'theme' }]
   },
   {
     id: 're', nm: '房地产', ic: '🏠', ev: 'low', bk: 'BK0451', l2: [
-      { nm: '开发', cs: [{ c: '600048', n: '保利发展' }, { c: '000002', n: '万科A' }] }
-    ]
+      { nm: '开发', cs: [{ c: '600048', n: '保利发展' }, { c: '000002', n: '万科A' }] },
+    indices: [{ c: '399393', n: '国证地产', t: 'broad' }]
   },
   {
     id: 'auto', nm: '汽车', ic: '🚗', ev: 'mid', bk: 'BK0481', l2: [
       { nm: '整车', cs: [{ c: '002594', n: '比亚迪' }, { c: '601633', n: '长城汽车' }, { c: '600104', n: '上汽集团' }] },
-      { nm: '零部件', cs: [{ c: '002050', n: '三花智控' }, { c: '600741', n: '华域汽车' }] },
-    ]
+      { nm: '零部件', cs: [{ c: '002050', n: '三花智控' }, { c: '600741', n: '华域汽车' }] },,
+    indices: [{ c: '399996', n: '300可选', t: 'broad' }, { c: '930606', n: '新能源车', t: 'theme' }]
   },
   {
     id: 'elec', nm: '电子', ic: '💻', ev: 'high', bk: 'BK0447', l2: [
@@ -179,40 +179,40 @@ export const INDUSTRIES: Industry[] = [
   },
   {
     id: 'coal', nm: '煤炭', ic: '⛏️', ev: 'high', bk: 'BK0437', l2: [
-      { nm: '煤炭开采', cs: [{ c: '601088', n: '中国神华' }, { c: '600188', n: '兖矿能源' }] }
-    ]
+      { nm: '煤炭开采', cs: [{ c: '601088', n: '中国神华' }, { c: '600188', n: '兖矿能源' }] },
+    indices: [{ c: '399998', n: '中证煤炭', t: 'broad' }]
   },
   {
     id: 'oil', nm: '石油石化', ic: '🛢️', ev: 'mid', bk: 'BK0464', l2: [
-      { nm: '油气', cs: [{ c: '601857', n: '中国石油' }, { c: '600028', n: '中国石化' }] }
-    ]
+      { nm: '油气', cs: [{ c: '601857', n: '中国石油' }, { c: '600028', n: '中国石化' }] },
+    indices: [{ c: '000042', n: '中证全指能源', t: 'broad' }]
   },
   {
     id: 'chem', nm: '化工', ic: '🧪', ev: 'mid', bk: 'BK0462', l2: [
-      { nm: '化学制品', cs: [{ c: '600309', n: '万华化学' }, { c: '002601', n: '龙佰集团' }] }
-    ]
+      { nm: '化学制品', cs: [{ c: '600309', n: '万华化学' }, { c: '002601', n: '龙佰集团' }] },
+    indices: [{ c: '000043', n: '中证全指化工', t: 'broad' }]
   },
   {
     id: 'steel', nm: '钢铁', ic: '🔩', ev: 'mid', bk: 'BK0478', l2: [
-      { nm: '普钢', cs: [{ c: '600019', n: '宝钢股份' }, { c: '000932', n: '华菱钢铁' }] }
-    ]
+      { nm: '普钢', cs: [{ c: '600019', n: '宝钢股份' }, { c: '000932', n: '华菱钢铁' }] },
+    indices: [{ c: '000043', n: '中证全指材料', t: 'broad' }]
   },
   {
     id: 'metal', nm: '有色金属', ic: '🥇', ev: 'mid', bk: 'BK0479', l2: [
       { nm: '工业金属', cs: [{ c: '601899', n: '紫金矿业' }, { c: '603993', n: '洛阳钼业' }] },
       { nm: '贵金属', cs: [{ c: '600547', n: '山东黄金' }] },
-      { nm: '能源金属', cs: [{ c: '002460', n: '赣锋锂业' }] },
-    ]
+      { nm: '能源金属', cs: [{ c: '002460', n: '赣锋锂业' }] },,
+    indices: [{ c: '000043', n: '中证全指材料', t: 'broad' }]
   },
   {
     id: 'build', nm: '建筑装饰', ic: '🏗️', ev: 'mid', bk: 'BK0424', l2: [
-      { nm: '基建', cs: [{ c: '601668', n: '中国建筑' }, { c: '601186', n: '中国铁建' }] }
-    ]
+      { nm: '基建', cs: [{ c: '601668', n: '中国建筑' }, { c: '601186', n: '中国铁建' }] },
+    indices: [{ c: '000043', n: '中证全指材料', t: 'broad' }]
   },
   {
     id: 'bmat', nm: '建筑材料', ic: '🧱', ev: 'low', bk: 'BK0425', l2: [
-      { nm: '水泥', cs: [{ c: '600585', n: '海螺水泥' }] }
-    ]
+      { nm: '水泥', cs: [{ c: '600585', n: '海螺水泥' }] },
+    indices: [{ c: '000043', n: '中证全指材料', t: 'broad' }]
   },
   {
     id: 'appl', nm: '家用电器', ic: '📺', ev: 'mid', bk: 'BK0456', l2: [
@@ -222,8 +222,8 @@ export const INDUSTRIES: Industry[] = [
   },
   {
     id: 'text', nm: '纺织服饰', ic: '👔', ev: 'low', bk: 'BK0436', l2: [
-      { nm: '服装', cs: [{ c: '600398', n: '海澜之家' }] }
-    ]
+      { nm: '服装', cs: [{ c: '600398', n: '海澜之家' }] },
+    indices: [{ c: '000043', n: '中证全指材料', t: 'broad' }]
   },
   {
     id: 'trans', nm: '交通运输', ic: '🚢', ev: 'mid', bk: 'BK0429', l2: [
@@ -252,34 +252,34 @@ export const INDUSTRIES: Industry[] = [
   },
   {
     id: 'beauty', nm: '美容护理', ic: '💄', ev: 'mid', bk: 'BK0440', l2: [
-      { nm: '化妆品', cs: [{ c: '603605', n: '珀莱雅' }] }
-    ]
+      { nm: '化妆品', cs: [{ c: '603605', n: '珀莱雅' }] },
+    indices: [{ c: '000043', n: '中证全指材料', t: 'broad' }]
   },
   {
     id: 'social', nm: '社会服务', ic: '🍽️', ev: 'mid', bk: 'BK0450', l2: [
-      { nm: '酒店', cs: [{ c: '600258', n: '首旅酒店' }] }
-    ]
+      { nm: '酒店', cs: [{ c: '600258', n: '首旅酒店' }] },
+    indices: [{ c: '000043', n: '中证全指消费', t: 'broad' }]
   },
   {
     id: 'light', nm: '轻工制造', ic: '📦', ev: 'low', bk: 'BK0453', l2: [
-      { nm: '家居', cs: [{ c: '603816', n: '顾家家居' }, { c: '002572', n: '索菲亚' }] }
-    ]
+      { nm: '家居', cs: [{ c: '603816', n: '顾家家居' }, { c: '002572', n: '索菲亚' }] },
+    indices: [{ c: '000043', n: '中证全指材料', t: 'broad' }]
   },
   {
     id: 'env', nm: '环保', ic: '🌿', ev: 'high', bk: 'BK0426', l2: [
-      { nm: '治理', cs: [{ c: '300070', n: '碧水源' }] }
-    ]
+      { nm: '治理', cs: [{ c: '300070', n: '碧水源' }] },
+    indices: [{ c: '000827', n: '中证环保', t: 'broad' }]
   },
   {
     id: 'retail', nm: '商贸零售', ic: '🛒', ev: 'mid', bk: 'BK0454', l2: [
-      { nm: '百货', cs: [{ c: '600859', n: '王府井' }] }
-    ]
+      { nm: '百货', cs: [{ c: '600859', n: '王府井' }] },
+    indices: [{ c: '000043', n: '中证全指消费', t: 'broad' }]
   },
   {
     id: 'machinery', nm: '机械设备', ic: '⚙️', ev: 'mid', bk: 'BK0452', l2: [
       { nm: '工程机械', cs: [{ c: '600031', n: '三一重工' }, { c: '000422', n: '湖北宜化' }] },
-      { nm: '液压', cs: [{ c: '601100', n: '恒立液压' }] }
-    ]
+      { nm: '液压', cs: [{ c: '601100', n: '恒立液压' }] },
+    indices: [{ c: '000043', n: '中证全指工业', t: 'broad' }]
   },
   {
     id: 'comp2', nm: '综合', ic: '🔷', ev: 'mid', bk: 'BK0471', l2: []
